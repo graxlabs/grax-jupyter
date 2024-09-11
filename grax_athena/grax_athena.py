9054ba2f6e9cd62d89b2425d3bd83d58f79f3644
Chris Continanza <christopher.continanza@gmail.com>
2024-09-11 12:41:05 -0400

    use workgroup param

--- grax_athena/grax_athena.py
+++ grax_athena/grax_athena.py
@@ -8,13 +8,14 @@ import pandas as pd
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 SCHEMA_NAME = os.getenv('ATHENA_DATABASE')
-S3_STAGING_DIR = os.getenv('S3_STAGING_DIR')
 AWS_REGION = os.getenv('AWS_REGION') 
+AWS_WORKGROUP = os.getenv('AWS_WORKGROUP', 'primary')
+S3_STAGING_DIR = os.getenv('S3_STAGING_DIR')
 
 conn_str = (
     "awsathena+rest://{aws_access_key_id}:{aws_secret_access_key}@"
     "athena.{region_name}.amazonaws.com:443/"
-    "{schema_name}?s3_staging_dir={s3_staging_dir}&work_group=primary"
+    "{schema_name}?s3_staging_dir={s3_staging_dir}&work_group={work_group}"
 )
 
 def athena_connection():
@@ -25,6 +26,7 @@ def athena_connection():
           aws_secret_access_key=quote_plus(AWS_SECRET_KEY),
           region_name=AWS_REGION,
           schema_name=SCHEMA_NAME,
+          work_group=AWS_WORKGROUP,
           s3_staging_dir=quote_plus(S3_STAGING_DIR),
       )
   )
